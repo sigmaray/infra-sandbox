c83dba039f7ae623d0d5fcd73500d19b0907e181
Stanislav Zaleski <sigmaray@gmail.com>
2026-06-27 19:21:26 +0300

    Improve caddy tests

--- tests/caddy.spec.ts
+++ tests/caddy.spec.ts
@@ -2,36 +2,28 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Caddy Reverse Proxy', () => {
   test('Drupal is accessible via subdomain on port 80', async ({ request }) => {
-    const response = await request.get('http://127.0.0.1/', {
-      headers: { Host: 'drupal.localhost' }
-    });
+    const response = await request.get('http://drupal.localhost/');
     expect(response.ok()).toBeTruthy();
     const body = await response.text();
     expect(body).toMatch(/Drupal|My Drupal Site/i);
   });
 
   test('FreshRSS is accessible via subdomain on port 80', async ({ request }) => {
-    const response = await request.get('http://127.0.0.1/', {
-      headers: { Host: 'freshrss.localhost' }
-    });
+    const response = await request.get('http://freshrss.localhost/');
     expect(response.ok()).toBeTruthy();
     const body = await response.text();
     expect(body).toMatch(/FreshRSS/i);
   });
 
   test('Static Server (feeds) is accessible via subdomain on port 80', async ({ request }) => {
-    const response = await request.get('http://127.0.0.1/manifest.json', {
-      headers: { Host: 'feeds.localhost' }
-    });
+    const response = await request.get('http://feeds.localhost/manifest.json');
     expect(response.ok()).toBeTruthy();
     const body = await response.json();
     expect(body).toHaveProperty('feeds');
   });
 
   test('Go Blog is accessible via subdomain on port 80', async ({ request }) => {
-    const response = await request.get('http://127.0.0.1/', {
-      headers: { Host: 'blog.localhost' }
-    });
+    const response = await request.get('http://blog.localhost/');
     expect(response.ok()).toBeTruthy();
   });
 });
